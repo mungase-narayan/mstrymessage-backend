e3088d842d2e3ff1c965df4d497901d0a2bd00a2
mungase-narayan <narayanmungase03@gmail.com>
2024-09-02 14:45:27 +0530

    add route for get messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function GET(request: Request){
+    await dbConnect();
+
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "User NOT authenticated",
+            },
+            { status: 401 }
+        );
+    }
+
+    const userId = user._id;
+}
